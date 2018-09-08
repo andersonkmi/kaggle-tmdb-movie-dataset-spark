fb404bd8925b83736d7bc1e2dd05407181dd611c
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2018-09-08 10:23:52 -0300

    S3 download implementation

--- src/main/scala/com/iterium/tmdb/BaseDataSetHandler.scala
+++ src/main/scala/com/iterium/tmdb/BaseDataSetHandler.scala
@@ -4,8 +4,11 @@ import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 trait BaseDataSetHandler {
-  val ColumnNames: List[String]
+  val CSVZipFileName: String = "tmdb-5000-movie-dataset.zip"
+  val TmdbMoviesFileName: String = "tmdb_5000_movies.csv"
+  val TmdbCreditsFileName: String = "tmdb_5000_credits.csv"
 
+  val ColumnNames: List[String]
   def readContents(file: String, session: SparkSession): DataFrame = {
     session.read.format("com.databricks.spark.csv").schema(getSchema(ColumnNames)).option("header", "true").option("quote", "\"").option("escape", "\"").load(file)
   }
